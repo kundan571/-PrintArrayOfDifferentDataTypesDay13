1db022c4249586fd1aa17755b9de3edb39f67c46
kundan571 <kundanchauhan1997@gmail.com>
2023-02-21 01:05:33 +0530

    Added: Welcome Message

---
+++ src/com/differentdatatypesofarray/PrintDifferentDataTypesOfArray.java
@@ -0,0 +1,7 @@
+package com.differentdatatypesofarray;
+
+public class PrintDifferentDataTypesOfArray {
+    public static void main(String[] args) {
+        System.out.println("Welcome To  Print Array of Different Data Types Program");
+    }
+}
