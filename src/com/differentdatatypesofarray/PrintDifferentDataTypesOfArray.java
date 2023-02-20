0587973c53a85266234436030a7e2c8e76d543ac
kundan571 <kundanchauhan1997@gmail.com>
2023-02-21 01:42:19 +0530

    Added: Printed different data types With Generics

--- src/com/differentdatatypesofarray/PrintDifferentDataTypesOfArray.java
+++ src/com/differentdatatypesofarray/PrintDifferentDataTypesOfArray.java
@@ -1,34 +1,22 @@
 package com.differentdatatypesofarray;
 
 public class PrintDifferentDataTypesOfArray {
-    public static void toPrintInteger(Integer[] Array){
-        for (int i : Array){
-            System.out.print(i + ", ");
+    public static <E> void toPrint(E[] Array){
+        for (E element : Array){
+            System.out.print(element + ", ");
         }
         System.out.println();
     }
-    public static void toPrintDouble(Double[] Array){
-        for (double i : Array){
-            System.out.print(i + ", ");
-        }
-        System.out.println();
-    }
-    public static void toPrintCharacter(Character[] Array){
-        for (char i : Array){
-            System.out.print(i + ", ");
-        }
-        System.out.println();
-    }
-
     public static void main(String[] args) {
         Integer[] intArray = {1, 2, 3, 4, 5, 6};
         Double[] doubleArray = {1.1, 3.2, 5.9, 4.6};
         Character[] charArray = {'K', 'U', 'N', 'D', 'A', 'N'};
         PrintDifferentDataTypesOfArray printDifferentDataTypesOfArray = new PrintDifferentDataTypesOfArray();
 
-        printDifferentDataTypesOfArray.toPrintInteger(intArray);
-        printDifferentDataTypesOfArray.toPrintDouble(doubleArray);
-        printDifferentDataTypesOfArray.toPrintCharacter(charArray);
+        printDifferentDataTypesOfArray.toPrint(intArray);
+        printDifferentDataTypesOfArray.toPrint(doubleArray);
+        printDifferentDataTypesOfArray.toPrint(charArray);
 
     }
+
 }
