467fd26518f3a8293864c2ca3bbc3d091232553b
kundan571 <kundanchauhan1997@gmail.com>
2023-02-21 02:01:37 +0530

    Added: Printed different data types With Generics Class

--- src/com/differentdatatypesofarray/PrintDifferentDataTypesOfArray.java
+++ src/com/differentdatatypesofarray/PrintDifferentDataTypesOfArray.java
@@ -1,6 +1,13 @@
 package com.differentdatatypesofarray;
 
-public class PrintDifferentDataTypesOfArray {
+public class PrintDifferentDataTypesOfArray<T> {
+    private T[] Array;
+    public PrintDifferentDataTypesOfArray(T[] Array){
+        this.Array = Array;
+    }
+    public void toPrint(){
+        PrintDifferentDataTypesOfArray.toPrint(this.Array);
+    }
     public static <E> void toPrint(E[] Array){
         for (E element : Array){
             System.out.print(element + ", ");
@@ -11,7 +18,7 @@ public class PrintDifferentDataTypesOfArray {
         Integer[] intArray = {1, 2, 3, 4, 5, 6};
         Double[] doubleArray = {1.1, 3.2, 5.9, 4.6};
         Character[] charArray = {'K', 'U', 'N', 'D', 'A', 'N'};
-        PrintDifferentDataTypesOfArray printDifferentDataTypesOfArray = new PrintDifferentDataTypesOfArray();
+        PrintDifferentDataTypesOfArray printDifferentDataTypesOfArray = new PrintDifferentDataTypesOfArray(intArray);
 
         printDifferentDataTypesOfArray.toPrint(intArray);
         printDifferentDataTypesOfArray.toPrint(doubleArray);
